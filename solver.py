7373181a1c90fb9b47548c743c241cbcd1c86856
jchernof17 <jordan.chernof@gmail.com>
2020-04-21 18:26:52 -0400

    fix skeleton code, add strategy wrapper function, build example output files

--- solver.py
+++ solver.py
@@ -1,7 +1,9 @@
 import networkx as nx
 from parse import read_input_file, write_output_file
 from utils import is_valid_network, average_pairwise_distance
+from generator import generate_tree
 import sys
+import time
 
 
 def solve(G):
@@ -13,9 +15,35 @@ def solve(G):
         T: networkx.Graph
     """
 
-    # TODO: your code here!
-    pass
+    return generate_tree(10)
 
+
+def run_solver(full=True):
+    """
+    Runs the solve() function on all graphs in the inputs folder and saves outputs
+    """
+    # start timer
+    start_time = time.perf_counter()
+    # find files
+    sizes = ["small", "medium", "large"]
+    num_graphs = [303, 303, 400]
+    # we can just do small graphs if the parameter full is set to False
+    if not full:
+        num_graphs = num_graphs[0:1]
+
+    # loop through all inputs and create outputs
+    for i in range(len(num_graphs)):
+        for j in range(1, num_graphs[i] + 1):
+            filepath = sizes[i]+"-"+str(j)
+            G = read_input_file("inputs/"+filepath+".in")
+            T = solve(G)
+            write_output_file(T, "outputs/"+filepath+".out")
+    end_time = time.perf_counter()
+    print(f"Process complete. Total time {end_time - start_time:0.4f} seconds")
+
+
+if __name__ == '__main__':
+    run_solver()
 
 # Here's an example of how to run your solver.
 
