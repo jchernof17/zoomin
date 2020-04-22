104fedea44d955b699bc05463d045bc70d9924c1
jchernof17 <jordan.chernof@gmail.com>
2020-04-21 23:19:37 -0400

    no longer completely naive

--- solver.py
+++ solver.py
@@ -15,23 +15,31 @@ def solve(G):
         T: networkx.Graph
     """
     MST = nx.minimum_spanning_tree(G)
-    best_score = average_pairwise_distance_fast(MST)
+    best_T = MST.copy()
+    best_score = average_pairwise_distance_fast(best_T)
     # if a node of T has degree 1, let's delete it and calculate the pairwise distance
-    BEST_MST = MST
-    leaves = [key[0] for key in MST.degree() if key[1] == 1].sort(reverse=True)
-    if leaves:
-        for leaf in leaves:
-            BEST_MST.remove_node(leaf)
-            potential_score = average_pairwise_distance_fast(BEST_MST)
-            if potential_score > best_score:
-                BEST_MST = MST  # change it back
-            else:
-                MST = BEST_MST  # change our MST
 
-    return BEST_MST
+    sources_and_sinks = [pair[0] for pair in MST.degree() if pair[1] == 1]  # get vertices with degree 1
+    deletable_edges = [e for e in MST.edges if e[0] in sources_and_sinks or e[1] in sources_and_sinks]
+    for edge in deletable_edges:
+        new_T = best_T.copy()
+        new_T.remove_edge(*edge)
+        if not new_T[edge[0]] and len(new_T) > 1 and len(MST[edge[0]]) == 1:
+            new_T.remove_node(edge[0])
+        if not new_T[edge[1]] and len(new_T) > 1 and len(MST[edge[1]]) == 1:
+            new_T.remove_node(edge[1])
+        if not new_T.edges:
+            new_score = 0
+        else:
+            new_score = average_pairwise_distance_fast(new_T)
+        if new_score <= best_score:
+            best_T = new_T
+            best_score = new_score
+
+    return best_T
 
 
-def run_solver(full=True):
+def run_solver(full=True, file=""):
     """
     Runs the solve() function on all graphs in the inputs folder and saves outputs
     """
@@ -45,12 +53,18 @@ def run_solver(full=True):
         num_graphs = num_graphs[0:1]
 
     # loop through all inputs and create outputs
-    for i in range(len(num_graphs)):
-        for j in range(1, num_graphs[i] + 1):
-            filepath = sizes[i]+"-"+str(j)
-            G = read_input_file("inputs/"+filepath+".in")
-            T = solve(G)
-            write_output_file(T, "outputs/"+filepath+".out")
+    if not file:
+        for i in range(len(num_graphs)):
+            for j in range(1, num_graphs[i] + 1):
+                filepath = sizes[i]+"-"+str(j)
+                G = read_input_file("inputs/"+filepath+".in")
+                T = solve(G)
+                write_output_file(T, "outputs/"+filepath+".out")
+    else:
+        filepath = file
+        G = read_input_file("inputs/"+filepath+".in")
+        T = solve(G)
+        write_output_file(T, "outputs/"+filepath+".out")
     end_time = time.perf_counter()
     print(f"Process complete. Total time {end_time - start_time:0.4f} seconds")
 
